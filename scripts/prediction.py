b2f9749c89d55b4651edc8a177ba6ae1e47a4dc1
ysc <26206118+YannSc@users.noreply.github.com>
2020-12-16 19:57:12 +0100

    load two configs for benchmark, NMS to be disabled

--- scripts/prediction.py
+++ scripts/prediction.py
@@ -13,31 +13,46 @@ args = parser.parse_args()
 
 # We reload the config to get everything as in the experiment
 sys.path.append(join(args.experiment, "config"))
-from config.pascalvoc.ssd.dct.dct_07.config_file import TrainingConfiguration
-config = TrainingConfiguration()
+from config.pascalvoc.ssd.dct.dct_07.config_file import TrainingConfiguration as TC_DCT
+config_dct = TC_DCT()
+
+from config.pascalvoc.ssd.rgb.rgb_07.config_file import TrainingConfiguration as TC_RGB
+config_rgb = TC_RGB()
+
+def prep_model(config):
 
 # Loading the model, for prepare for inference if required
-config.prepare_for_inference()
-config.network.load_weights(args.weights)
+    config.prepare_for_inference()
+    config.network.load_weights(args.weights)
+    model = config.network
+    model.compile(loss=config.loss,
+                  optimizer=config.optimizer,
+                  metrics=config.metrics)
 
-model = config.network
+    config.prepare_testing_generator()
+    config.test_generator.shuffle = True
+    return model
 
-model.compile(loss=config.loss,
-              optimizer=config.optimizer,
-              metrics=config.metrics)
-
-config.prepare_testing_generator()
-config.test_generator.shuffle = True
-
+model_dct = prep_model(config_dct)
 # Getting the batch to process
-X, _ = config.test_generator.__getitem__(0)
-
+X, _ = config_dct.test_generator.__getitem__(0)
 # If the input is not a displayable stuff, get the displayable
-X_true, y = config.test_generator.get_raw_input_label(0)
+X_true, y = config_dct.test_generator.get_raw_input_label(0)
 
-y_pred = model.predict(X)
+print("Start DCT inference test")
+i = 0
+while (i<2000):
 
-if args.groundTruth:
-    config.displayer.display_with_gt(y_pred, X_true, y)
-else:
-    config.displayer.display(y_pred, X_true)
+    y_pred = model_dct.predict(X)
+
+
+model_rgb = prep_model(config_rgb)
+X, _ = config_dct.test_generator.__getitem__(0)
+# If the input is not a displayable stuff, get the displayable
+X_true, y = config_dct.test_generator.get_raw_input_label(0)
+
+print("Start RGB inference test")
+i = 0
+while (i<2000):
+
+    y_pred = model_dct.predict(X)
